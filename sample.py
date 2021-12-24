379e537a590b0f80005c4b341011912eff9d9a2d
Ag-2O <sakuramoti3079@gmail.com>
2021-12-24 12:29:59 +0900

    kansei

--- sample.py
+++ sample.py
@@ -3,26 +3,31 @@ import pickle
 import numpy as np
 from matplotlib import pyplot as plt
 
+np.set_printoptions(precision=4)
+
 def get_diagonal(eig_value, eig_vec, value_list):
 
-    """ koyuuti to koyuubekutoru no kumi wo kaesu """
+    """ 固有値と固有ベクトルを返す """
 
     for idx in range(len(eig_value)):
-        value_list.append([eig_value[idx], np.hstack(eig_vec[i][idx] for i in range(len(eig_value)))])
-        #print("vec_shape: {}".format(eig_vec[idx].shape))
+        value_list.append([eig_value[idx], np.hstack(eig_vec[:,idx])])
+        #print("vec_shape: {}".format(eig_vec.shape))
     return value_list
 
-def save_subspace(W):
 
-    """ bubunn kuukann no hozonn """
+def save_subspace(W,name):
 
-    np.save("msm",W)
+    """ 部分空間の保存 """
 
-def load_subspace():
+    np.save(name,W)
 
-    """ bubunn kuukann no yomikomi """
 
-    return np.load("msm.npy")
+def load_subspace(name):
+
+    """ 部分空間の読み込み """
+
+    return np.load("py_imgrecog/subspace_default/"+ name +".npy")
+
 
 def adjust_data(X_train,y_train,X_test,y_test,label_list,test_label_list):
 
@@ -30,6 +35,7 @@ def adjust_data(X_train,y_train,X_test,y_test,label_list,test_label_list):
 
     train_data = []
     for ll in range(len(label_list)):
+        """
         # 訓練データは3つダブってるので
         i = ll * 3
         j = i + 1
@@ -40,110 +46,257 @@ def adjust_data(X_train,y_train,X_test,y_test,label_list,test_label_list):
         b = X_train[j]
         c = X_train[k]
         data = np.hstack([a,b,c])
+        """
+        
 
-        #print("group: {}".format(group))
-        #print("length: {}".format(data.shape))
+        data = X_train[ll]
 
         # 正規化
         group_data = []
         for idx in range(data.shape[1]):
-            x = data[:,idx]
-            x_l2_norm = sum(x**2)**0.5
-            x_l2_normalized = x / x_l2_norm
-            group_data.append(x_l2_normalized)
+            x = data[:,idx] / np.linalg.norm(data[:,idx])
+            group_data.append(x)
         train_data.append(group_data)
-    print("訓練データを調整した！")
 
     """ テストデータの調整 """
 
     test_data = []
     for ll in range(len(test_label_list)):
-        # tesuto データ
+        """
+        # テストデータは6つダブってるので
+        i = ll * 3
+        j = i + 1
+        k = j + 1
+        l = k + 1
+        m = l + 1
+        n = m + 1
+
+        # データの連結
+        a = X_test[i]
+        b = X_test[j]
+        c = X_test[k]
+        d = X_test[l]
+        e = X_test[m]
+        f = X_test[n]
+
+        data = np.hstack([a,b,c,d,e,f])
+        """
+
         data = X_test[ll]
 
         #print("group: {}".format(group))
         #print("length: {}".format(data.shape))
 
+        
         # 正規化
         group_data = []
         for idx in range(data.shape[1]):
-            x = data[:,idx]
-            x_l2_norm = sum(x**2)**0.5
-            x_l2_normalized = x / x_l2_norm
-            group_data.append(x_l2_normalized)
+            x = data[:,idx] / np.linalg.norm(data[:,idx])
+            group_data.append(x)
         test_data.append(group_data)
-    print("テストデータを調整した！")
+        
+        """
+        # 動作テスト用
+        group_data = []
+        for idx in range(10):
+            x = data[:,idx] / np.linalg.norm(data[:,idx])
+            group_data.append(x)
+        test_data.append(group_data)
+        """
 
+    print("訓練データサイズ: {}  テストデータサイズ: {}".format(len(train_data),len(test_data)))
     return train_data, test_data
 
-def create_subspace(train_data,dim):
+
+def create_subspace(train_data,dim,label_list):
     
-    """ 部分関数を求める """
+    """ 部分空間を求める """
 
-    W = np.empty([47,dim,dim])
-    print("W.shape: {}".format(W.shape))
+    W = np.empty([len(label_list),dim,dim])
 
-    #print("len_train_data: {}".format(len(train_data)))
     count = 0
-    for group in train_data:
+    for g in range(len(train_data)):
         print("count: {}".format(count))
         #print("len_group: {}".format(len(group)))
         #print("len_data: {}".format(len(data)))
-        for i, data in enumerate(group):
-            value_list = []
+        R = np.empty([dim,dim])
+        for i, data in enumerate(train_data[g]):
             X = data
+            value_list = []
             #print("shape_x: {}".format(X.reshape(1,-1)))
             #print("shape_x.T: {}".format(X.reshape(-1,1)))
-            C = X.reshape(-1,1)*X.reshape(1,-1)
+            R += X.reshape(-1,1)*X.reshape(1,-1)
             #print("C: {}".format(C))
-            eig_value, eig_vec = np.linalg.eig(C)
-            #print("eig_value_size: {}, eig_vec_size: {}".format(eig_value.shape,eig_vec.shape))
-            value_list = get_diagonal(eig_value, eig_vec, value_list)
-            value_list = sorted(value_list,key=lambda x: x[0], reverse=True)
-            #print("value_list: {}".format(value_list[1]))
-            #print("sample_vec: {}, length: {}".format(value_list[1:30][1][1], len(value_list[1:30][1][1])))
-            W[count,:,:] = value_list[1:100][1][1]
+
+        eig_value, eig_vec = np.linalg.eig(R)
+        value_list = get_diagonal(eig_value, eig_vec, value_list)
+
+        #print("eig_value_size: {}, eig_vec_size: {}".format(eig_value.shape,eig_vec.shape))
+        #value_list = get_diagonal(eig_value, eig_vec, value_list)
+        value_list = sorted(value_list,key=lambda x: x[0], reverse=True)
+        
+        v = np.array([x[0] for x in value_list])
+        vec = np.array([x[1] for x in value_list])
+
+        #print("value: {}".format(v))
+        #print("vec: {}".format(vec))
+        #print("vec.shape: {}".format(vec.shape))
+        #print("value_list: {}".format(value_list))
+        #print("sample_vec: {}, \n length: {}".format(value_list[1:100][1][1], len(value_list[1][1])))
+        W[count,:,:] = vec
         #print("W.shape: {}".format(W.shape))
         count += 1
-    print("部分関数を求めた！")
-
+    print("部分空間を求めた！")
     return W
+
 
 def run_clafic(test_data, label_list, test_label_list,W):
 
     """ CLAFICによる分類 """
 
-    # yomikomi
-    W = load_subspace()
-
-    r = 100
-
-    # bunnrui
+    # 分類
     count_ok = 0
     count = 0
+    # データセット全体
     for i, group in enumerate(test_data):
+        # データセット内の1つ1つのデータ
         for j, data in enumerate(group):
-            out = np.empty(label_dim)
-            for label in range(label_dim):
+            out = np.empty(len(label_list))
+            # 部分空間のラベルの数分繰り返し
+            for label in range(len(label_list)):
                 #print("test_num: {}, label_num: {}, data_num: {}".format(i,label,j))
-                # sikibetu no siki 
-                out[label] = np.max((W[label:,:,1:r].T * test_data[i][j])**2) / (np.linalg.norm(W[label:,:,1:r].T)**2 * np.linalg.norm(test_data[i][j])**2)
+
+                # 部分空間法
+                #out[label] = np.linalg.norm(W[label,:,:].T * test_data[i][j])**2
+
+                
+                S = 0
+                for r in range(100):
+                    S += np.dot(test_data[i][j],W[label,r,:])**2 / (np.linalg.norm(test_data[i][j]) * np.linalg.norm(W[label,r,:]))
+                out[label] = S
+                
+
+                # 識別の式
+                #out[label] = np.max( ((W[label,:,:].T * test_data[i][j])**2) / (np.linalg.norm(W[label,:,:].T)**2 * np.linalg.norm(test_data[i][j])**2) )
                 #print("W: {}".format(W[label,:,:].T))
 
+            print("-------------------------- {} --------------------------".format(count))
             print("out: \n {}".format(out))
             print("test_num: {}, data_num: {}".format(i,j))
             print("arg_max_out: {}".format(np.argmax(out)))
             print("predict_label: {}".format(label_list[np.argmax(out)]))
             print("label: {}".format(test_label_list[i]))
-            print("-------------------------- {} --------------------------".format(count))
 
             if label_list[np.argmax(out)] == test_label_list[i]:
-                print("accurate!")
+                print("correct!")
+                count_ok += 1
+            count += 1
+
+            # 正解率
+            print("accuracy: {}".format(count_ok/count))
+
+def run_msm(test_data, label_list, test_label_list,W,Q):
+
+    """ 相互部分空間法 """
+
+    # 分類
+    count_ok = 0
+    count = 0
+    # データセット全体
+    for i, group in enumerate(test_data):
+        # データセット内の1つ1つのデータ
+        for j, data in enumerate(group):
+            out = np.empty(len(label_list))
+            # 部分空間のラベルの変更
+            for label in range(len(label_list)):
+                #print("test_num: {}, label_num: {}, data_num: {}".format(i,label,j))
+
+                # 識別の式
+                #print("W.shape: {}, Q.shape: {}".format(W[label].T.shape, Q[i].shape))
+                #print("dot: {}".format(np.sum(np.dot(W[label,:,:], Q[i,:,:]))))
+                #out[label] = (np.sum(np.dot(W[label,:,:], Q[i,:,:]))**2) / (np.linalg.norm(W[label,:,:])**2 * np.linalg.norm(Q[i,:,:])**2)
+                #out[label] = np.max((W[label,:,:].T * Q[i,:,:])**2 / (np.linalg.norm(W[label,:,:])**2 * np.linalg.norm(Q[i,:,:])**2))
+                #print("W: {}".format(W[label,:,:].T))
+                
+                #C = W[label,:,:].T * Q[i,:,:] * Q[i,:,:].T * W[label,:,:]
+                #eig_val, eig_vec = np.linalg.eig(C)
+                #out[label] = np.min(eig_val)
+
+                
+                
+                S = 0
+                for r in range(100):
+                    S += np.dot(W[label,r,:].T, Q[i,r,:])**2 / (np.linalg.norm(W[label,r,:])**2 * np.linalg.norm(Q[i,r,:])**2)
+                out[label] = S
+                
+                
+                
+
+            print("-------------------------- {} --------------------------".format(count))
+            print("out: \n {}".format(out))
+            print("test_num: {}, data_num: {}".format(i,j))
+            print("arg_max_out: {}".format(np.argmax(out)))
+            print("predict_label: {}".format(label_list[np.argmax(out)]))
+            print("label: {}".format(test_label_list[i]))
+
+            if label_list[np.argmax(out)] == test_label_list[i]:
+                print("correct!")
+                count_ok += 1
+            count += 1
+
+            # 正解率
+            print("accuracy: {}".format(count_ok/count))
+
+def run_ex_msm(test_data, label_list, test_label_list,W,Q):
+
+    """ 改良した相互部分空間法 """
+
+    # 分類
+    count_ok = 0
+    count = 0
+    # データセット全体
+    for i, group in enumerate(test_data):
+        # データセット内の1つ1つのデータ
+        for j, data in enumerate(group):
+            out = np.empty(len(label_list))
+            # 部分空間のラベルの変更
+            for label in range(len(label_list)):
+                #print("test_num: {}, label_num: {}, data_num: {}".format(i,label,j))
+
+                # 識別の式
+                #out[label] = np.max( ((W[label,:,:].T * Q[i,:,:])**2) / (np.linalg.norm(W[label,:,:].T)**2 * np.linalg.norm(Q[i,:,:])**2) )
+                #print("W: {}".format(W[label,:,:].T))
+
+                S = 0
+                for r in range(100):
+                    S += np.dot(W[label,r,:].T, Q[i,r,:])**2 / (np.linalg.norm(W[label,r,:].T)**2 * np.linalg.norm(Q[i,r,:])**2)
+                out[label] = S
+
+            print("-------------------------- {} --------------------------".format(count))
+            print("out: \n {}".format(out))
+            print("test_num: {}, data_num: {}".format(i,j))
+            print("arg_max_out: {}".format(np.argmax(out)))
+            print("predict_label: {}".format(label_list[np.argmax(out)]))
+            print("label: {}".format(test_label_list[i]))
+
+            if label_list[np.argmax(out)] == test_label_list[i]:
+                print("correct!")
                 count_ok += 1
             count += 1
 
     # 正解率
     print("accuracy: {}".format(count_ok/count))
+
+
+def test_vector(W):
+
+    """ 固有値と固有ベクトルを確かめる """
+
+    W = load_subspace("msm")
+
+    print("W.shape: {}".format(W.shape))
+    print("W[1,:]: {}".format(W[1,:]))
+    print("W[2,:] : {}".format(W[2,:]))
 
 
 """ main """
@@ -165,46 +318,53 @@ if __name__ == "__main__":
     test_size = len(X_test)
     dim = X_train[0].shape[0]
 
-    # kunnrenn ラベルデータの生成
+    # 学習のラベルデータの生成
     label_list = []
     for i in range(len(y_train)):
-        if y_train[i] not in label_list:
-            label_list.append(y_train[i])
-
+        label_list.append(y_train[i])
     label_dim = len(label_list)
-    #print("label_list: {}, length: {}".format(label_list,len(label_list)))
+    print("label_list_length: {}".format(len(label_list)))
+    #print("label_list: {}".format(label_list))
 
-    # test raberu de-ta no seisei
+    # テストのラベルデータの生成
     test_label_list = []
     for i in range(len(y_test)):
         test_label_list.append(y_test[i])
     test_label_dim = len(test_label_list)
+    print("test_label_list_length: {}".format(len(test_label_list)))
+    #print("test_label_list: {}".format(test_label_list))
 
     # 画像の表示
     #plt.imshow(X_train[0][:, 10].reshape((20, 20)))
     #plt.show()
 
-    # de-ta no tyousei
+    # データの調整
     train_data, test_data = adjust_data(X_train,y_train,X_test,y_test,label_list,test_label_list)
+
+    #W = create_subspace(train_data,dim)
 
     try:
         # load 
-        W = load_subspace()
+        W = load_subspace("msm")
+        Q = load_subspace("test")
         print("load subspace!")
 
     except:
-        # data ga naitoki
+        # データが無いとき
         print("create subspace!")
 
-        # bubunn kuukann no dousyutu
-        W = create_subspace(train_data,dim)
-        print("subspace created!")
+        # 部分空間を求める
+        W = create_subspace(train_data,dim,label_list)
+        save_subspace(W,"msm")
 
-        # hozonn
-        save_subspace(W)
-        
-        # load 
-        W = load_subspace()
+        # テストデータの部分空間
+        Q = create_subspace(test_data,dim,test_label_list)
+        save_subspace(Q,"test")
+
+        print("subspace created!")
     
-    # bunnrui
-    run_clafic(test_data,label_list,test_label_list,W)
+    #test_vector(W)
+
+    # 分類
+    #run_clafic(test_data,label_list,test_label_list,W)
+    run_msm(test_data,label_list,test_label_list,W,Q)
